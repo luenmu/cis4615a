a841bca8fb6e4b7c22375d99e5acd867f5ddb44d
luenmu <85597824+luenmu@users.noreply.github.com>
2023-10-07 15:55:04 -0400

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -3,10 +3,10 @@
 IDS03-J. Do not log unsanitized user input Given the non-compliant code below:
  ******************************************************************************/
 
-i f ( l o g i n S u c c e s s f u l ) {
-l o g g e r . s e v e r e (” User l o g i n s u c c e e d e d f o r : ” + username ) ;
-} e l s e {
-l o g g e r . s e v e r e (” User l o g i n f a i l e d f o r : ” + username ) ;
+if (loginSuccessful) {
+  logger.severe("User login succeeded for: " + username);
+} else {
+  logger.severe("User login failed for: " + username);
 }
 
 
