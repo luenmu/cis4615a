01c52799549a77e1b62bce7c42320bc352b3ddb1
luenmu <85597824+luenmu@users.noreply.github.com>
2023-10-08 19:49:17 -0400

    Update R00_IDS03_J.java

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,7 +1,7 @@
 /******************************************************************************
  1. Rule 00. Input Validation and Data Sanitization (IDS)
 IDS03-J. Do not log unsanitized user input.
-Correct the code as shown in theCompliant Solution below:
+Given the non-compliant code below:
  ******************************************************************************/
 
 if (loginSuccessful) {
