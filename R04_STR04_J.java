6534efb8f8c8b6f6ca50afaf37076a41e89f9f0b
luenmu <85597824+luenmu@users.noreply.github.com>
2023-10-08 18:14:26 -0400

    Create R04_STR04_J.java

---
+++ R04_STR04_J.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+4. Rule 04. Characters and Strings (STR)
+STR04-J. Use compatible character encodings when communicating string data between JVMs.
+Given the non-compliant code below:
+ ******************************************************************************/
+
+
+FileInputStream fis = null;
+try {
+  fis = new FileInputStream("SomeFile");
+  DataInputStream dis = new DataInputStream(fis);
+  byte[] data = new byte[1024];
+  dis.readFully(data);
+  String result = new String(data);
+} catch (IOException x) {
+  // Handle error
+} finally {
+  if (fis != null) {
+    try {
+      fis.close();
+    } catch (IOException x) {
+      // Forward to handler
+    }
+  }
+}
